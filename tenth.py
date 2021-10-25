767f2fcc9083e61135c558432da9d1634a642e57
AR-Thube <91319379+AR-Thube@users.noreply.github.com>
2021-10-25 23:43:24 +0530

    Update tenth.py

--- tenth.py
+++ tenth.py
@@ -2,7 +2,11 @@
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
 
+
+#Taking input from the user.
 num = int(input("Enter a number: "))
+
+#Passing conditions for checking if the number given is even or odd.
 if (num % 2) == 0:
    print("{0} is Even".format(num))
 else:
